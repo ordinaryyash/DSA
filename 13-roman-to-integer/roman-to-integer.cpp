155f0bf16e036f26273872cf1246431ad737a937
Atharv Bhandarkar <atharvabh22@gmail.com>
2025-08-30 19:38:22 +0530

    Time: 15 ms (9.93%) | Memory: 13.3 MB (50.44%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     int romanToInt(string s) {
+        unordered_map<char, int> values = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
         int sum = 0;
         int n = s.size();
-        for (int i = 0; i < n; i++) {
-            int val = 0;
-            if (s[i] == 'I') val = 1;
-            else if (s[i] == 'V') val = 5;
-            else if (s[i] == 'X') val = 10;
-            else if (s[i] == 'L') val = 50;
-            else if (s[i] == 'C') val = 100;
-            else if (s[i] == 'D') val = 500;
-            else if (s[i] == 'M') val = 1000;
-            if (i + 1 < n) {
-                int nextVal = 0;
-                if (s[i+1] == 'I') nextVal = 1;
-                else if (s[i+1] == 'V') nextVal = 5;
-                else if (s[i+1] == 'X') nextVal = 10;
-                else if (s[i+1] == 'L') nextVal = 50;
-                else if (s[i+1] == 'C') nextVal = 100;
-                else if (s[i+1] == 'D') nextVal = 500;
-                else if (s[i+1] == 'M') nextVal = 1000;
 
-                if (val < nextVal) {
-                    sum -= val;
-                    continue;
-                }
+        for (int i = 0; i < n; i++) {
+            // If current value is smaller than next value, subtract
+            if (i + 1 < n && values[s[i]] < values[s[i + 1]]) {
+                sum -= values[s[i]];
+            } else {
+                sum += values[s[i]];
             }
-            sum =sum + val;
         }
+
         return sum;
     }
 };
